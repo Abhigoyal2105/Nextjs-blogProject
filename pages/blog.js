6c6ecd59fd0e03b5ba1714b66439e408669a54a7
abhishekgoyal <abhishekgoyal2105@gmail.com>
2024-01-30 17:30:29 +0530

    Creating Multiple Pages in Next.js | NextJs Tutorial for Beginners #8

---
+++ pages/blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>This is my blog component</div>
+  )
+}
+
+export default blog
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>This is my Contact component</div>
+  )
+}
+
+export default contact
