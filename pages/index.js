7fa521fbc1de78216201577d2f6cc7f950d69168
abhishekgoyal <abhishekgoyal2105@gmail.com>
2024-01-30 17:00:12 +0530

    test

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
           <li>Home</li>
           <li>About</li>
           <li>Blog</li>
-          <li>contact</li>
+          <li>Contact</li>
         </ul>
       </nav>
       <main className={`${styles.main} ${inter.className}`}>
